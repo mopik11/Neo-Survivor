e5f0f3fd89463b2256db470eddd18b1c4272f3ae
mopik11 <matej.pacl@gmail.com>
2026-06-21 12:43:28 +0200

    v1.487: Fix empty black space in mobile modals and support safe-area insets for notches

--- main.js
+++ main.js
@@ -4515,7 +4515,7 @@ function initSocket() {
 
         NET.socket.on('joined', (data) => {
             const { roomId, playerState } = data;
-            console.log("=== NEO SURVIVOR v1.486 ===");
+            console.log("=== NEO SURVIVOR v1.487 ===");
             NET.roomId = roomId;
             NET.isMultiplayer = true;
             document.querySelectorAll('.modal').forEach(m => m.classList.remove('active'));
