157d19d217ff8b0d531810b229323dbea0d6b2c9
mopik11 <matej.pacl@gmail.com>
2026-07-23 13:47:59 +0200

    Auto-update: Nová Cloudflare URL -> https://api.trycloudflare.com

--- main.js
+++ main.js
@@ -5284,7 +5284,7 @@ function initSocket() {
     // Automatická detekce serveru (lokální vs produkční)
     const SERVER_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? "http://localhost:3000"
-        : "https://sponsors-met-offset-vsnet.trycloudflare.com";
+        : "https://api.trycloudflare.com";
 
     try {
         NET.socket = io(SERVER_URL, {
