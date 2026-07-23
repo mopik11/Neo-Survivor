89a4c2931bb321949ecd42611f61bcfad0b7e91e
mopik11 <matej.pacl@gmail.com>
2026-07-23 13:50:53 +0200

    Auto-update: Nová Cloudflare URL -> https://sponsors-met-offset-vsnet.trycloudflare.com

--- main.js
+++ main.js
@@ -5284,7 +5284,7 @@ function initSocket() {
     // Automatická detekce serveru (lokální vs produkční)
     const SERVER_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? "http://localhost:3000"
-        : "https://radius-church-hop-dig.trycloudflare.com";
+        : "https://sponsors-met-offset-vsnet.trycloudflare.com";
 
     try {
         NET.socket = io(SERVER_URL, {
