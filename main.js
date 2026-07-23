50777390b5de2127d8ff7176849be0393bd594ae
mopik11 <matej.pacl@gmail.com>
2026-07-23 13:54:01 +0200

    Auto-update: Nová Cloudflare URL -> https://saying-hartford-gain-oscar.trycloudflare.com

--- main.js
+++ main.js
@@ -5284,7 +5284,7 @@ function initSocket() {
     // Automatická detekce serveru (lokální vs produkční)
     const SERVER_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? "http://localhost:3000"
-        : "https://api.trycloudflare.com";
+        : "https://saying-hartford-gain-oscar.trycloudflare.com";
 
     try {
         NET.socket = io(SERVER_URL, {
