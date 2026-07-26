1d0e5c241d1cdd53ac2272d0291cb1d0b54b8a6d
mopik11 <matej.pacl@gmail.com>
2026-07-26 15:10:09 +0200

    Auto-update: New Cloudflare URL

--- main.js
+++ main.js
@@ -5289,7 +5289,7 @@ function initSocket() {
     // Automatická detekce serveru (lokální vs produkční)
     const SERVER_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? "http://localhost:3000"
-        : "https://islamic-reproduce-dancing-hollywood.trycloudflare.com";
+        : "https://manufactured-felt-require-ocean.trycloudflare.com";
 
     try {
         NET.socket = io(SERVER_URL, {
