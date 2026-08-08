c6b282686c2b29e2507da5f38b7fae0ae49e437a
mopik11 <matej.pacl@gmail.com>
2026-08-08 16:35:47 +0000

    Auto-update: New Cloudflare URL

--- main.js
+++ main.js
@@ -5289,7 +5289,7 @@ function initSocket() {
     // Automatická detekce serveru (lokální vs produkční)
     const SERVER_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? "http://localhost:3000"
-        : "https://rebound-hall-dad-collapse.trycloudflare.com";
+        : "https://much-disco-wal-petroleum.trycloudflare.com";
 
     try {
         NET.socket = io(SERVER_URL, {
